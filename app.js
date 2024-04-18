01548989cafac8f85d0834d467b033356d116591
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-04-18 15:21:44 +0300

    Done

--- app.js
+++
@@ -1,5 +0,0 @@
-var multiple = function (a, b) {
-    return a * (b !== null && b !== void 0 ? b : 1);
-};
-console.log(multiple(1, 2));
-console.log(multiple(3));
--- app.ts
+++ app.ts
@@ -1,23 +1,38 @@
-// void в TypeScript обозначает, что функция не возвращает никакого значения.
+let input: unknown;
 
-function logId(id: number | string): void {
-  console.log(id);
+input = 3;
+input = [1, 2, 3];
+
+function run(i: unknown) {
+  if (typeof i === "number") {
+    console.log(i * 2);
+  } else if (typeof i === "string") {
+    console.log(i.toLocaleLowerCase());
+  }
 }
 
-// можем вернуть все что угодно, но в результате этот возврат будет игнорироваться.
-type voidFunction = () => void;
+run(input);
 
-const f1: voidFunction = () => {};
-const f2: voidFunction = () => true;
+// кейс
+async function getData() {
+  try {
+    await fetch("");
+  } catch (e) {
+    if (e instanceof Error) {
+      console.log(e.message);
+    }
+  }
+}
 
-const b = f2();
+async function getDataForce() {
+  try {
+    await fetch("");
+  } catch (e) {
+    // явный каст, но все может упасть
+    const er = e as Error;
+    console.log(er.message);
+  }
+}
 
-const skills = ["html", "css", "js"];
-
-const user: {
-  skills: string[];
-} = {
-  skills: [],
-};
-
-skills.forEach((skill) => user.skills.push(skill));
+type U1 = unknown | null; // union type с unknown всегда становится unknown
+type I1 = unknown & number; // intersection type с unknown всегда другой тип
