d12e8c372dd87ae298ffd3f4d18fb026eeb7cc12
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-04-18 18:27:57 +0300

    Done

--- app.ts
+++ app.ts
@@ -1,19 +1,31 @@
-const n: null = null;
-const a: any = null;
+let a: number = 5;
+let b: string = String(a);
+let c: number = Number(b);
 
 interface User {
   name: string;
+  email: string;
+  login: string;
 }
 
-function getUser() {
-  if (Math.random() > 0.5) {
-    return null;
-  } else {
-    return {
-      name: "John",
-    } as User;
-  }
+const user: User = {
+  name: "John",
+  email: "j@j.com",
+  login: "j@j.com",
+};
+
+interface Admin {
+  name: string;
+  role: string;
 }
 
-const user = getUser();
-const n55 = user?.name;
+// неправильно и не удобно
+const admin: Admin = {
+  ...user,
+  role: "admin",
+};
+
+// правильно
+function userToAdmin(user: User): Admin {
+  return { name: user.name, role: "admin" };
+}
